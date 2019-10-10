d78545f83dddaaa84386240681ea2d34306ce946
aidan melville <amelville@lcs.on.ca>
2019-10-10 11:09:39 -0400

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Melville, Aidan on 2019-10-10.
+//  Copyright © 2019 Melville, Aidan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
